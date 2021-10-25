9739d9261a989ad2e77c7a625c1dcec0a7054bfe
Jamara1 <johanamara@fundacionview.org>
2021-10-25 16:01:38 -0500

    Mobile application
    Installs:
    - Pgadmin 4
    - Composer
    - Laravel
    
    Developed:
    - Database, users table
    - User Interface
    - Splash Screen

---
+++ Android/app/src/main/java/com/example/android/SplashScreen.java
@@ -0,0 +1,20 @@
+package com.example.android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+
+@SuppressLint("CustomSplashScreen")
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+
+        //startActivity(new Intent(this, LoginActivity.class));
+    }
+}

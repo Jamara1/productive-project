219c3a4320552270c3d00a8d3e6e36a98cb28a1f
Jamara1 <johanamara@fundacionview.org>
2021-10-26 14:02:57 -0500

    Recognition of each element of the login, validation and creation of the table in the database, called users taking into account the given guidelines

--- Android/app/src/main/java/com/example/android/LoginActivity.java
+++ Android/app/src/main/java/com/example/android/LoginActivity.java
@@ -3,13 +3,72 @@ package com.example.android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.CheckBox;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.Objects;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private SharedPreferences.Editor editor;
+    private TextInputLayout lyUsername, lyPsw;
+    private TextInputEditText username, psw;
+    private String strUsername, strPsw;
+    private CheckBox remember;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        SharedPreferences preferences = getSharedPreferences("data", MODE_PRIVATE);
+        editor = preferences.edit();
+
+        lyUsername = findViewById(R.id.ly_username);
+        lyPsw = findViewById(R.id.ly_psw);
+
+        username = findViewById(R.id.edt_username);
+        psw = findViewById(R.id.edt_psw);
+
+        remember = findViewById(R.id.cb_remember);
+    }
+
+    private void getData() {
+        strUsername = Objects.requireNonNull(username.getText()).toString();
+        strPsw = Objects.requireNonNull(psw.getText()).toString();
+    }
+
+    private void logOn() {
+        if (remember.isChecked()) {
+            editor.putBoolean("status", true);
+            editor.apply();
+        }
+        startActivity(new Intent(this, HomeActivity.class));
+    }
+
+    private void validator() {
+        getData();
+
+        if (strUsername.isEmpty()) {
+            lyUsername.setErrorEnabled(true);
+            lyUsername.setError("The username is obligatory!");
+        } else {
+            lyUsername.setErrorEnabled(false);
+            lyUsername.setError(null);
+        }
+
+        if (strPsw.isEmpty()) {
+            lyPsw.setErrorEnabled(true);
+            lyPsw.setError("The password is obligatory!");
+        } else {
+            lyPsw.setErrorEnabled(false);
+            lyPsw.setError(null);
+        }
+
+        if (lyUsername.getError() == null && lyPsw.getError() == null) logOn();
     }
 }
